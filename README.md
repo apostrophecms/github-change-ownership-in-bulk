658aa6ea41566ae2a8e0dca2fe87a3b86805a8a5
Tom Boutell <tom@punkave.com>
2017-12-11 17:42:43 -0500

    it works

---
+++ README.md
@@ -0,0 +1,27 @@
+Change ownership of a whole bunch of github repos in a hurry.
+
+```
+npm install
+cp config-example.js config.js
+vi config.js
+[change the token to YOUR token]
+node app
+```
+
+Boom! It's very fast.
+
+If it encounters any errors it will stop; see the output to figure out which one failed (the last one), fix your tpo and comment out those prior to it, as they will fail too on the next try if they have already been moved successfully.
+
+Yes, github leaves a trail of redirects behind. Don't recreate the repos in the original org though; that will break the redirects.
+
+No, silly, there is NO warranty. Use at your own risk. It worked for us when we needed to move zillyuns of Apostrophe modules.
+
+Copyright (c) 2017 P'unk Avenue LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
---
+++ app.js
@@ -0,0 +1,61 @@
+var GitHubApi = require("github");
+var _ = require('lodash');
+var config = require('./config.js');
+var fs = require('fs');
+var argv = require('boring')();
+var request = require('request-promise');
+var Promise = require('bluebird');
+
+Promise.map(config.transfers, function(transfer) {
+  return Promise.try(function() {
+    return request('https://api.github.com/orgs/' + transfer.to + '/teams', {
+      headers: {
+        accept: 'application/vnd.github.nightshade-preview+json',
+        'user-agent': 'github-change-ownership-in-bulk'
+      },
+      json: true,
+      qs: {
+        access_token: config.token
+      }
+    });
+  })
+  .then(function(teams) {
+    var teamIds = [];
+    _.each(transfer.teams, function(team) {
+      var actualTeam = _.find(teams, function(actualTeam) {
+        return (actualTeam.name === team) || (actualTeam.slug === team);
+      });
+      if (!actualTeam) {
+        throw new Error('Cannot find team ' + team + ' in the organization ' + transfer.to);
+      }
+      teamIds.push(actualTeam.id);
+    });
+    return Promise.map(transfer.repos, function(repo) {
+      console.log('transferring ' + repo + ' from ' + config.from + ' to ' + transfer.to);
+      return request('https://api.github.com/repos/' + config.from + '/' + repo + '/transfer', {
+        method: 'POST',
+        headers: {
+          accept: 'application/vnd.github.nightshade-preview+json',
+          'user-agent': 'github-change-ownership-in-bulk'
+        },
+        qs: {
+          access_token: config.token
+        },
+        json: true,
+        body: {
+          new_owner: transfer.to,
+          team_id: [ teamIds ]
+        }
+      });
+    });
+  });
+})
+.then(function() {
+  console.log('Done');
+});
+
+function testrequest() {
+  console.log(JSON.stringify(arguments, null, '  '));
+  return Promise.resolve(true);
+}
+
